maths22/brlcad

commit 60a8d73039165c71d590d2a2570d2c3865e496f6

remove the mirror_pt 'distance' parameter from rt_mirror() as it just unnecessarily complicates the interface.  point+dir are sufficient.  testing seems to indicate no change in functionality so hopefully we're 'good to go'
git-svn-id: svn://svn.code.sf.net/p/brlcad/code/brlcad/trunk@34263 2f96ce8b-6d43-0410-b8df-bffccc660ffb

src/librt/primitives/mirror.c
@@ -46,9 +46,8 @@
 struct rt_db_internal *
 rt_mirror(struct db_i *dbip,
 	  struct rt_db_internal *ip,
-	  point_t mirror_origin,
+	  point_t mirror_pt,
 	  vect_t mirror_dir,
-	  fastf_t mirror_pt,
 	  struct resource *resp)
 {
     register int i, j;
@@ -96,25 +95,10 @@ rt_mirror(struct db_i *dbip,
     MAT_COPY(temp, mirmat);
     bn_mat_mul(mirmat, temp, rmat);
 
-    /* Factor in the mirror_origin */
-    {
-	vect_t v;
-
-	VSUB2(v, origin, mirror_origin);
-	VUNITIZE(v);
-
-	if (NEAR_ZERO(MAGSQ(v) - 1.0, tol_dist_sq)) {
-	    fastf_t h = MAGNITUDE(mirror_origin);
-	    fastf_t cosa = VDOT(v, mirror_dir);
-
-	    mirror_pt = mirror_pt - h * cosa;
-	}
-    }
-
     /* Add the translation to mirmat */
-    mirmat[3 + X*4] += 2 * mirror_pt * mirror_dir[X];
-    mirmat[3 + Y*4] += 2 * mirror_pt * mirror_dir[Y];
-    mirmat[3 + Z*4] += 2 * mirror_pt * mirror_dir[Z];
+    mirmat[3 + X*4] += mirror_pt[X] * mirror_dir[X];
+    mirmat[3 + Y*4] += mirror_pt[Y] * mirror_dir[Y];
+    mirmat[3 + Z*4] += mirror_pt[Z] * mirror_dir[Z];
 
     switch (id) {
 	case ID_TOR:
@@ -278,13 +262,11 @@ rt_mirror(struct db_i *dbip,
 		    point_t ptB;
 		    point_t ptC;
 		    vect_t h;
-		    vect_t v;
 		    fastf_t mag;
 		    fastf_t cosa;
 
 		    VSCALE(ptA, n1, haf->eqn[H]);
-		    VSCALE(v, mirror_dir, 2 * mirror_pt);
-		    VADD2(ptB, ptA, v);
+		    VADD2(ptB, ptA, mirror_dir);
 		    VSUB2(h, ptB, ptA);
 		    mag = MAGNITUDE(h);
 		    VUNITIZE(h);

src/libged/mirror.c
@@ -40,8 +40,6 @@ ged_mirror(struct ged *gedp, int argc, const char *argv[])
     struct rt_db_internal *ip;
     struct rt_db_internal internal;
 
-    int early_out = 0;
-
     GED_CHECK_DATABASE_OPEN(gedp, BRLCAD_ERROR);
     GED_CHECK_READ_ONLY(gedp, BRLCAD_ERROR);
     GED_CHECK_ARGC_GT_0(gedp, argc, BRLCAD_ERROR);
@@ -65,14 +63,14 @@ ged_mirror(struct ged *gedp, int argc, const char *argv[])
 			   &mirror_dir[Y],
 			   &mirror_dir[Z]) != 3) {
 		    bu_vls_printf(&gedp->ged_result_str, "Usage: %s %s", argv[0], usage);
-		    early_out = 1;
+		    return BRLCAD_ERROR;
 		}
 		break;
 	    case 'p':
 	    case 'P':
 		if (sscanf(bu_optarg, "%lf", &mirror_pt) != 1) {
 		    bu_vls_printf(&gedp->ged_result_str, "Usage: %s %s", argv[0], usage);
-		    early_out = 1;
+		    return BRLCAD_ERROR;
 		}
 		break;
 	    case 'o':
@@ -82,7 +80,7 @@ ged_mirror(struct ged *gedp, int argc, const char *argv[])
 			   &mirror_origin[Y],
 			   &mirror_origin[Z]) != 3) {
 		    bu_vls_printf(&gedp->ged_result_str, "Usage: %s %s", argv[0], usage);
-		    early_out = 1;
+		    return BRLCAD_ERROR;
 		}
 		break;
 	    case 'x':
@@ -103,13 +101,9 @@ ged_mirror(struct ged *gedp, int argc, const char *argv[])
 		return BRLCAD_HELP;
 	    default:
 		bu_vls_printf(&gedp->ged_result_str, "Usage: %s %s", argv[0], usage);
-		early_out = 1;
+		return BRLCAD_ERROR;
 		break;
 	}
-    }
-
-    if (early_out) {
-	return BRLCAD_ERROR;
     }
 
     argc -= bu_optind;
@@ -121,44 +115,46 @@ ged_mirror(struct ged *gedp, int argc, const char *argv[])
 
     /* make sure object mirroring to does not already exist */
     if (db_lookup(gedp->ged_wdbp->dbip, argv[bu_optind+1], LOOKUP_QUIET) != DIR_NULL) {
-	bu_vls_printf(&gedp->ged_result_str, "ERROR: %s already exists\n", argv[bu_optind+1]);
+	bu_vls_printf(&gedp->ged_result_str, "%s already exists\n", argv[bu_optind+1]);
 	return BRLCAD_ERROR;
     }
 
     /* look up the object being mirrored */
     if ((dp = db_lookup(gedp->ged_wdbp->dbip, argv[bu_optind], LOOKUP_NOISY)) == DIR_NULL) {
+	bu_vls_printf(&gedp->ged_result_str, "Unable to find solid [%s]\n", argv[bu_optind]);
 	return BRLCAD_ERROR;
     }
 
     /* get object being mirrored */
     ret = rt_db_get_internal(&internal, dp, gedp->ged_wdbp->dbip, NULL, gedp->ged_wdbp->wdb_resp);
     if (ret < 0) {
-	bu_vls_printf(&gedp->ged_result_str, "ERROR: Unable to load solid [%s]\n", argv[bu_optind]);
+	bu_vls_printf(&gedp->ged_result_str, "Unable to load solid [%s]\n", argv[bu_optind]);
 	return BRLCAD_ERROR;
     }
 
     mirror_pt *= gedp->ged_wdbp->dbip->dbi_local2base;
+    VUNITIZE(mirror_dir);
+    VJOIN1(mirror_origin, mirror_origin, mirror_pt, mirror_dir);
     
     /* mirror the object */
     ip = rt_mirror(gedp->ged_wdbp->dbip,
 		   &internal,
 		   mirror_origin,
 		   mirror_dir,
-		   mirror_pt,
 		   gedp->ged_wdbp->wdb_resp);
     if (ip == NULL) {
-	bu_vls_printf(&gedp->ged_result_str, "ERROR: Unable to mirror %s", argv[0]);
+	bu_vls_printf(&gedp->ged_result_str, "Unable to mirror [%s]", argv[bu_optind]);
 	return BRLCAD_ERROR;
     }
 
     /* add the mirrored object to the directory */
     if ((dp = db_diradd(gedp->ged_wdbp->dbip, argv[bu_optind+1], -1L, 0, dp->d_flags, (genptr_t)&ip->idb_type)) == DIR_NULL) {
-	bu_vls_printf(&gedp->ged_result_str, "ERROR: Unable to add [%s] to the database directory", argv[bu_optind+1]);
+	bu_vls_printf(&gedp->ged_result_str, "Unable to add [%s] to the database directory", argv[bu_optind+1]);
 	return BRLCAD_ERROR;
     }
     /* save the mirrored object to disk */
     if (rt_db_put_internal(dp, gedp->ged_wdbp->dbip, ip, gedp->ged_wdbp->wdb_resp) < 0) {
-	bu_vls_printf(&gedp->ged_result_str, "ERROR: Unable to store [%s] to the database", argv[bu_optind+1]);
+	bu_vls_printf(&gedp->ged_result_str, "Unable to store [%s] to the database", argv[bu_optind+1]);
 	return BRLCAD_ERROR;
     }
 

include/raytrace.h
@@ -3280,9 +3280,8 @@ RT_EXPORT BU_EXTERN(void db_functree,
 RT_EXPORT BU_EXTERN(struct rt_db_internal *rt_mirror,
 		    (struct db_i *dpip,
 		     struct rt_db_internal *ip,
-		     point_t mirror_origin,
+		     point_t mirror_pt,
 		     vect_t mirror_dir,
-		     fastf_t mirror_pt,
 		     struct resource *resp));
 
 /*

commit 04f6ac7b8a61ca39891a556e4dc90779b774cf3c

that 2.0 scaling factor was rather important so that the object is translated across the mirror point far enough.  fixes a bug introduced with the r34263 elimination of the offset as a separately tracked value.

src/librt/primitives/mirror.c
@@ -132,9 +132,9 @@ rt_mirror(struct db_i *dbip,
     bn_mat_mul(mirmat, temp, rmat);
 
     /* Add the translation to mirmat */
-    mirmat[3 + X*4] += mirror_pt[X] * mirror_dir[X];
-    mirmat[3 + Y*4] += mirror_pt[Y] * mirror_dir[Y];
-    mirmat[3 + Z*4] += mirror_pt[Z] * mirror_dir[Z];
+    mirmat[3 + X*4] += 2.0 * mirror_pt[X] * mirror_dir[X];
+    mirmat[3 + Y*4] += 2.0 * mirror_pt[Y] * mirror_dir[Y];
+    mirmat[3 + Z*4] += 2.0 * mirror_pt[Z] * mirror_dir[Z];
 
     switch (id) {
 	case ID_TOR: